f92f2fc819afbc3969544a6a68e9ac727a9db8cc
MD Waliullah <waliullah.godev@gmail.com>
2025-08-16 08:20:19 +0000

    explore linear search

--- search/linear.c
+++ search/linear.c
@@ -3,6 +3,11 @@
 int linear_search(int A[], int n, int x)
 {
 
+
+    // time complexity -> O(n) -> only for n
+    // space complexity -> O(1) -> only for reaceived array
+
+
     int i;
 
     for (i = 0; i < n; i++)
