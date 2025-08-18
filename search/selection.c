2aab058a261d0de3e157d8208dd5d504d2625a3a
MD Waliullah <waliullah.godev@gmail.com>
2025-08-18 13:58:03 +0000

    added a picture for visualization

--- search/selection.c
+++ search/selection.c
@@ -3,9 +3,8 @@
 void selectionSort(int arr[], int n) {
     int i, j, min_idx;
 
-    // One by one move boundary of unsorted subarray
-    for (i = 0; i < n - 1; i++) {
-        // Find the minimum element in the unsorted array
+    // O
+    for (i = 0; i < n - 1; i++) {  
         min_idx = i;
         for (j = i + 1; j < n; j++) {
             if (arr[j] < arr[min_idx])
