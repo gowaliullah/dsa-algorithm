3545c8f31497a4345cc2642240300f2e18eb7ea5
MD Waliullah <waliullah.godev@gmail.com>
2025-08-31 09:26:00 +0600

    pre order for traversal

--- tree/binary.c
+++ tree/binary.c
@@ -52,10 +52,22 @@ Node *create_tree() {
 }
 
 
-
+// pre order for traversal 
+void pre_order(Node *node) {
+    
+    printf("%d ", node->data);
+    if (node->left != NULL) {
+        pre_order(node->left);
+    }
+    if (node->right != NULL) {
+        pre_order(node->right);
+    }
+    
+}
 
 int main() {
     Node *root = create_tree();
-    printf("%d\n", root->data);
+    pre_order(root);
+    printf("\n");
     return 0;
 }
